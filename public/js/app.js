7f480d38682146c53b245e0bd5e0e5d4ae128676
Deval Bibodi <deval.b@sptr.co>
2020-01-27 18:30:25 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log('Client Side javascript file loaded!')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log(search)
+    const location = search.value
+    console.log(location)
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+          
+            if (data.error) {
+                messageOne.textContent = data.error
+            }
+            else {
+                
+                messageOne.textContent = data.forecast
+                messageTwo.textContent = data.location
+            }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// define paths for express configs
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (request, response) => {
+    response.render("index", {
+        title: 'Weather',
+        name: 'Deval'
+    })
+})
+
+app.get('/about', (request, response) => {
+    response.render("about", {
+        title: 'About Me',
+        name: 'Deval'
+    })
+})
+
+
+app.get('/help', (request, response) => {
+    response.render("help", {
+        title: 'Help',
+        message: 'Help message',
+        name: 'Deval'
+    })
+})
+
+app.get('/weather', (request, response) => {
+
+    if (!request.query.address) {
+        return response.send({
+            error: 'You must provide a address'
+        })
+    }
+    
+    geocode(request.query.address,(error,{latitude,longitude, location} = {}) =>{
+        if(error)
+        {
+            return response.send({error})
+        }
+
+        forecast(latitude,longitude,(error,forecastData) =>{
+            if(error)
+            {
+                return response.send({error})
+            }
+
+            response.send({
+                forecast:forecastData,
+                location,
+                address:request.query.address
+            })
+
+        })
+    })
+
+    
+})
+
+app.get('/products', (request, response) => {
+
+    if (!request.query.search) {
+        return response.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(request.query.search)
+    response.send({
+        products: []
+    })
+
+})
+
+
+app.get('/help/*', (request, response) => {
+    response.render("notfound", { title: "Help", message: "Help Article not found!", name: 'Deval' })
+})
+
+app.get('*', (request, response) => {
+    response.render("notfound", { title: "Page Not Found", message: "My 404 Page!", name: 'Deval' })
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
